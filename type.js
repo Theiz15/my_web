8bddc6a2710f756a7a42527c36cef0dc6c1a26cc
vo Thai <148705061+Theiz15@users.noreply.github.com>
2024-09-05 18:49:51 +0700

    pro

---
+++ type.js
@@ -0,0 +1,34 @@
+const backgrounds = [
+    {
+      min: 0,
+      max: 6,
+      src: "",
+      greeting: "Quiet Hours 😴"
+    },
+    {
+      min: 6,
+      max: 12,
+      src: "",
+      greeting: "It's going to be a beautiful day 😊"
+    },
+    {
+      min: 12,
+      max: 17,
+      src: "",
+      greeting: "Good afternoon"
+    },
+    {
+      min: 17,
+      max: 21,
+      src: "",
+      greeting: "Good evening"
+    },
+    {
+      min: 21,
+      max: 24,
+      src: "",
+      greeting: "Good night"
+    }
+  ];
+  const backgroundEl = document.getElementById("background");
+    const greetingEl = document.getElementById("greeting");
